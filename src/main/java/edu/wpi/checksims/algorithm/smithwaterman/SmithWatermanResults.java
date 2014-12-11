ebf0b4bfed59c370f5066f7c817925f1784907d7
Matthew Heon <matthew.heon@gmail.com>
2014-12-11 14:41:45 -0500

    Relicense to CDDL 1.0
    Signed-off-by: Matthew Heon <matthew.heon@gmail.com>

--- src/main/java/edu/wpi/checksims/algorithm/smithwaterman/SmithWatermanResults.java
+++ src/main/java/edu/wpi/checksims/algorithm/smithwaterman/SmithWatermanResults.java
@@ -1,13 +1,34 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * See LICENSE.txt included in this distribution for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at LICENSE.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ * Copyright (c) 2014 Matthew Heon and Dolan Murvihill
+ */
+
 package edu.wpi.checksims.algorithm.smithwaterman;
 
 import edu.wpi.checksims.token.TokenList;
 import edu.wpi.checksims.util.TwoDimArrayCoord;
 import edu.wpi.checksims.util.TwoDimIntArray;
 
-import static edu.wpi.checksims.util.Direction.*;
-
 import java.util.LinkedList;
 import java.util.List;
+
+import static edu.wpi.checksims.util.Direction.*;
 
 /**
  * Results from one iteration of the Smith-Waterman algorithm
